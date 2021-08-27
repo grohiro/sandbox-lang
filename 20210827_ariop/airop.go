8d5ea7d5f97a8aa79c96e29861746d31f17ab1d5
grohiro <khirokawa@growk.jp>
2021-08-27 09:46:34 +0900

    airop

---
+++ 20210827_ariop/airop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 1)
+	fmt.Println(1 * 10)
+	fmt.Println(10 / 3)
+	fmt.Println(3.3333 * 3.3333)
+	fmt.Println(3 * 1.23456)
+	fmt.Println(1.23456 * 3)
+}
+
+/*
+2
+10
+3
+11.11088889
+3.70368
+*/
---
+++ 20210827_ariop/airop.php
@@ -0,0 +1,21 @@
+<?php
+
+function println($s) {
+    echo $s;
+    echo "\n";
+}
+
+println(1 + 1);
+println(1 * 10);
+println(10 / 3);
+println(3.3333 * 3.3333);
+println(3 * 1.23456);
+println(1.23456 * 3);
+
+/*
+2
+10
+3.3333333333333
+11.11088889
+3.70368
+*/
