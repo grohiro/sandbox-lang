a357f7ac04107127d32236158a97043f6d7a28f3
grohiro <khirokawa@growk.jp>
2021-09-03 15:29:11 +0900

    bitwise

---
+++ 20210903_bitwise/bitwise.ts
@@ -0,0 +1,18 @@
+
+
+
+const OK      = 0b0001
+const READY   = 0b0010
+const STOP    = 0b0100
+
+for (let status of [OK, READY, STOP]) {
+  if (status & OK) {
+    console.log('OK');
+  }
+  if (status & READY) {
+    console.log('READY');
+  }
+  if (status & STOP) {
+    console.log('STOP');
+  }
+}
