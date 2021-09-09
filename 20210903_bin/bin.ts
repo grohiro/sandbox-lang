728001e92c50ebdc673891a8ba4354b5fb93c335
grohiro <khirokawa@growk.jp>
2021-09-09 17:37:03 +0900

    check_binary

--- 20210903_bin/bin.ts
+++ 20210903_bin/bin.ts
@@ -8,3 +8,10 @@ const l = [0b0, 0b1, 0b10, 0b11];
 for (n of l) {
   console.log({n});
 }
+
+console.log(0b0 === 0);
+console.log(0b1 === 1);
+console.log(0b10 === 2);
+
+console.log(String(0b10));
+
